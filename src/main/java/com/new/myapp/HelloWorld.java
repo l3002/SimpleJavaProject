eb642bf8416273d96a7b4170e7a45f9acaa2c158
Tanmay Mathpal <Tanmaymathpal4545@gmail.com>
2022-09-24 10:13:48 +0530

    Update HelloWorld.java

--- src/main/java/com/new/myapp/HelloWorld.java
+++ src/main/java/com/new/myapp/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("This is Simple Java Project");
+		System.out.println("This is Simple Java Project no this is not");
 	}
 }
